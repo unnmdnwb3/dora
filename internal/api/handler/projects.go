27a750f171a24cb3c2b286a1bdbaacb5777dfc7e
Janick Rueegger <jrueegger@gmail.com>
2022-11-15 11:38:20 +0100

    Add projects client and api

--- internal/api/handler/projects.go
+++ internal/api/handler/projects.go
@@ -1,14 +1,14 @@
-package v1
+package handler
 
 import (
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/unnmdnwb3/dora/internal/integration/gitlab"
+	"github.com/unnmdnwb3/dora/internal/integrations/gitlab"
 )
 
-func GetProjects(c *gin.Context) {
+func ReadAllProjects(c *gin.Context) {
 	client, err := gitlab.NewClient()
 	if err != nil {
 		log.Fatalln(err.Error())
--- main.go
+++ main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	v1 "github.com/unnmdnwb3/dora/internal/api/v1"
+	"github.com/unnmdnwb3/dora/internal/api/handler"
+	"github.com/unnmdnwb3/dora/internal/database/mongodb"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func main() {
+	ctx := context.Background()
+    client, err := mongodb.NewClient(&ctx)
+    if err != nil {
+		log.Fatalln(err)
+	}
+
+    err = client.Ping(context.TODO(), readpref.Primary())
+    if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Successfully connected to database.")
+	client.Disconnect(ctx)
+
     router := gin.Default()
-    router.GET("/api/v1/projects", v1.GetProjects)
+
+    router.GET("/api/v1/projects", handler.ReadAllProjects)
+
+	router.POST("/api/integrations", handler.CreateIntegration)
+    router.GET("/api/integrations", handler.ReadIntegration)
+	router.PUT("/api/integrations", handler.UpdateIntegration)
+	router.DELETE("/api/integrations", handler.DeleteIntegration)
 
     log.Println("The server is running and listening on localhost")
-    err := http.ListenAndServe(":8080", router)
+    err = http.ListenAndServe(":8080", router)
     if err != nil {
         log.Fatalln("The server encountered a fatal error: ", err)
     }
