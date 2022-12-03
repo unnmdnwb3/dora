0c4bf03db0c8c96b471d1d17bc933b0a92902f80
Janick Rueegger <jrueegger@gmail.com>
2022-12-03 21:25:24 +0100

    Refactor mongodb, so that structs don't need to implement all the db functionality every time

--- internal/database/mongodb/client.go
+++ internal/database/mongodb/client.go
@@ -7,28 +7,31 @@ import (
 	"os"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
-	// Client provides all functionality for MongoDB
-	// and gets instatiated in the main function
+	// Client provides the driver functionality for MongoDB.
+	// It gets initialized in the main function as a singleton.
 	Client *mongo.Client
 
-	// DB points to the standard database
+	// DB provides the connectivity to a MongoDB instance.
+	// It gets initialized in the main function as a singleton.
 	DB *mongo.Database
 )
 
 const (
-	// Timeout holds the general TimeOut
+	// Timeout defines the timeout for database.
 	Timeout = 5 * time.Second
 
-	// StandardDatabase defines the standard database MongoDB needs to connect to
-	StandardDatabase = "dora"
+	// DefaultDatabase defines the default database name.
+	DefaultDatabase = "dora"
 )
 
-// ConnectionString creates the URI needed to connect to a mongodb instance
+// ConnectionString creates the connection string with the full URI for a MongoDB instance.
 func ConnectionString() (string, error) {
 	// necessary
 	uri := os.Getenv("MONGODB_URI")
@@ -51,7 +54,7 @@ func ConnectionString() (string, error) {
 	return fmt.Sprintf("mongodb://%s%s%s", auth, uri, port), nil
 }
 
-// Init creates a new mongodb client and pointer to the database
+// Init a Client and DB connection to MongoDB as singletons.
 func Init(ctx *context.Context) error {
 	conn, err := ConnectionString()
 	if err != nil {
@@ -63,7 +66,109 @@ func Init(ctx *context.Context) error {
 		return fmt.Errorf("could not establish connection to database: %s", err.Error())
 	}
 
-	DB = Client.Database(StandardDatabase)
+	DB = Client.Database(DefaultDatabase)
+
+	return nil
+}
+
+// InsertOne inserts a document into a collection.
+func InsertOne(ctx context.Context, collection string, v any) error {
+	coll := DB.Collection(collection)
+	insertOneResult, err := coll.InsertOne(ctx, v)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.M{"_id": insertOneResult.InsertedID}
+	applicationResult := coll.FindOne(ctx, filter)
+	if applicationResult.Err() != nil {
+		return err
+	}
+
+	err = applicationResult.Decode(v)
+	return err
+}
+
+// Find finds many documents in a collection.
+func Find(ctx context.Context, collection string, filter bson.M, vs any) error {
+	coll := DB.Collection(collection)
+
+	cursor, err := coll.Find(ctx, filter)
+	if err != nil {
+		return err
+	}
+
+	err = cursor.All(ctx, vs)
+	return err
+}
+
+// FindOne finds a documents in a collection.
+func FindOne(ctx context.Context, collection string, filter bson.M, v any) error {
+	coll := DB.Collection(collection)
+
+	findOneResult := coll.FindOne(ctx, filter)
+	if findOneResult.Err() != nil {
+		return findOneResult.Err()
+	}
+
+	err := findOneResult.Decode(v)
+	return err
+}
+
+// FindOneByID finds a documents with a specific ID in a collection.
+func FindOneByID(ctx context.Context, collection string, ID string, v any) error {
+	objectID, err := primitive.ObjectIDFromHex(ID)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.M{"_id": objectID}
+	err = FindOne(ctx, collection, filter, v)
+
+	return err
+}
+
+// UpdateOne updates a document in a collection.
+func UpdateOne(ctx context.Context, collection string, ID string, v any) error {
+	coll := DB.Collection(collection)
+
+	objectID, err := primitive.ObjectIDFromHex(ID)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.M{"_id": objectID}
+	update := bson.M{"$set": v}
+	updateOneResult, err := coll.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return err
+	}
+
+	if updateOneResult.MatchedCount == 0 {
+		return fmt.Errorf("id for update not found: %s", ID)
+	}
+
+	return nil
+}
+
+// DeleteOne deletes a document in a collection.
+func DeleteOne(ctx context.Context, collection string, ID string) error {
+	coll := DB.Collection(collection)
+
+	objectID, err := primitive.ObjectIDFromHex(ID)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.M{"_id": objectID}
+	deleteResult, err := coll.DeleteOne(ctx, filter)
+	if err != nil {
+		return err
+	}
+
+	if deleteResult.DeletedCount == 0 {
+		return fmt.Errorf("id for delete not found: %s", ID)
+	}
 
 	return nil
 }
--- internal/database/mongodb/client_test.go
+++ internal/database/mongodb/client_test.go
@@ -5,9 +5,12 @@ import (
 	"os"
 	"testing"
 
+	"github.com/joho/godotenv"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/unnmdnwb3/dora/internal/database/mongodb"
+	"github.com/unnmdnwb3/dora/internal/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
@@ -16,38 +19,161 @@ func TestClient(t *testing.T) {
 	RunSpecs(t, "mongodb.Client Suite")
 }
 
-var _ = BeforeSuite(func() {
-	os.Setenv("MONGODB_URI", "127.0.0.1")
-	os.Setenv("MONGODB_PORT", "27017")
-	os.Setenv("MONGODB_USER", "user")
-	os.Setenv("MONGODB_PASSWORD", "password")
+var _ = BeforeEach(func() {
+	_ = godotenv.Load("./../../../test/.env")
+
+	ctx := context.Background()
+	mongodb.Init(&ctx)
 })
 
-var _ = AfterSuite(func() {
+var _ = AfterEach(func() {
 	os.Remove("MONGODB_URI")
 	os.Remove("MONGODB_PORT")
 	os.Remove("MONGODB_USER")
 	os.Remove("MONGODB_PASSWORD")
+
+	ctx := context.Background()
+	mongodb.DB.Drop(ctx)
+	defer mongodb.Client.Disconnect(ctx)
 })
 
 var _ = Describe("mongodb.Client", func() {
+	ctx := context.Background()
+
 	var _ = When("ConnectionString", func() {
-		It("can build the correct connection string", func() {
-			expectedConn := "mongodb://user:password@127.0.0.1:27017"
-			Expect(mongodb.ConnectionString()).To(Equal(expectedConn))
+		It("can build a connection string", func() {
+			conn := "mongodb://user:password@127.0.0.1:27017"
+			Expect(mongodb.ConnectionString()).To(Equal(conn))
 		})
 	})
 
 	var _ = When("Init", func() {
-		It("creates a new MongoDB client with connection to a database", func() {
+		It("creates a new client and connection to a MongoDB instance ", func() {
 			ctx := context.Background()
-			err := mongodb.Init(&ctx)
-			defer mongodb.Client.Disconnect(ctx)
 
-			Expect(err).To(BeNil())
 			Expect(mongodb.Client).To(Not(BeNil()))
 			Expect(mongodb.DB).To(Not(BeNil()))
 			Expect(mongodb.Client.Ping(ctx, readpref.Primary())).To(BeNil())
+		})
+	})
+
+	var _ = When("InsertOne", func() {
+		It("creates a new document in a collection", func() {
+			application := models.Application{
+				Auth: "bearertoken",
+				Type: "gitlab",
+				URI:  "https://gitlab.com",
+			}
+			err := mongodb.InsertOne(ctx, "application", &application)
+			Expect(err).To(BeNil())
+			Expect(application.ID).To(Not(BeNil()))
+		})
+	})
+
+	var _ = When("Find", func() {
+		It("finds many documents in a collection", func() {
+			application1 := models.Application{
+				Auth: "bearertoken",
+				Type: "gitlab",
+				URI:  "https://gitlab.onprem1.com",
+			}
+			application2 := models.Application{
+				Auth: "bearertoken",
+				Type: "gitlab",
+				URI:  "https://gitlab.onprem2.com",
+			}
+			application3 := models.Application{
+				Auth: "bearertoken",
+				Type: "gitlab",
+				URI:  "https://gitlab.onprem.com",
+			}
+			mongodb.InsertOne(ctx, "application", &application1)
+			mongodb.InsertOne(ctx, "application", &application2)
+			mongodb.InsertOne(ctx, "application", &application3)
+			Expect(application1.ID).To(Not(BeNil()))
+			Expect(application2.ID).To(Not(BeNil()))
+			Expect(application3.ID).To(Not(BeNil()))
+
+			var findApplications []models.Application
+			filter := bson.M{"type": "gitlab"}
+			err := mongodb.Find(ctx, "application", filter, &findApplications)
+			Expect(err).To(BeNil())
+			Expect(findApplications).To(HaveLen(3))
+		})
+	})
+
+	var _ = When("FindOne", func() {
+		It("finds a specific document in a collection", func() {
+			application := models.Application{
+				Auth: "bearertoken",
+				Type: "gitlab",
+				URI:  "https://gitlab.com",
+			}
+			mongodb.InsertOne(ctx, "application", &application)
+			Expect(application.ID).To(Not(BeNil()))
+
+			var findApplication models.Application
+			filter := bson.M{"uri": "https://gitlab.com"}
+			err := mongodb.FindOne(ctx, "application", filter, &findApplication)
+			Expect(err).To(BeNil())
+			Expect(findApplication.ID).To(Equal(application.ID))
+		})
+	})
+
+	var _ = When("FindOneByID", func() {
+		It("finds a specific document with ID in a collection", func() {
+			application := models.Application{
+				Auth: "bearertoken",
+				Type: "gitlab",
+				URI:  "https://gitlab.com",
+			}
+			mongodb.InsertOne(ctx, "application", &application)
+			Expect(application.ID).To(Not(BeNil()))
+
+			var findApplication models.Application
+			err := mongodb.FindOneByID(ctx, "application", application.ID, &findApplication)
+			Expect(err).To(BeNil())
+			Expect(findApplication.ID).To(Equal(application.ID))
+		})
+	})
+
+	var _ = When("UpdateOne", func() {
+		It("updates a document with ID in a collection", func() {
+			application := models.Application{
+				Auth: "bearertoken",
+				Type: "gitlab",
+				URI:  "https://gitlab.com",
+			}
+			mongodb.InsertOne(ctx, "application", &application)
+			Expect(application.ID).To(Not(BeNil()))
+
+			updateApplication := models.Application{
+				Auth: "newbearertoken",
+				Type: "gitlab",
+				URI:  "https://gitlab.com",
+			}
+			err := mongodb.UpdateOne(ctx, "application", application.ID, &updateApplication)
+			Expect(err).To(BeNil())
+			Expect(updateApplication.Auth).To(Equal("newbearertoken"))
+		})
+	})
+
+	var _ = When("DeleteOne", func() {
+		It("deletes a document with ID in a collection", func() {
+			application := models.Application{
+				Auth: "bearertoken",
+				Type: "gitlab",
+				URI:  "https://gitlab.com",
+			}
+			mongodb.InsertOne(ctx, "application", &application)
+			Expect(application.ID).To(Not(BeNil()))
+
+			err := mongodb.DeleteOne(ctx, "application", application.ID)
+			Expect(err).To(BeNil())
+
+			var findApplication models.Application
+			err = mongodb.FindOneByID(ctx, "application", application.ID, &findApplication)
+			Expect(err).To(Not(BeNil()))
 		})
 	})
 })
