2ac468b0ac6ea923abc2d7348e51e3e549635887
Janick Rueegger <jrueegger@gmail.com>
2022-12-13 20:19:32 +0100

    Enhance time utils to check for the same date of two times

--- internal/utils/time_test.go
+++ internal/utils/time_test.go
@@ -42,4 +42,21 @@ var _ = Describe("mongodb.Service", func() {
 			Expect(same).To(BeFalse())
 		})
 	})
+
+	var _ = When("Date", func() {
+		It("returns a time with the time set to 00:00:00.", func() {
+			ts := "2019-10-09T09:11:20.861Z"
+			t, err := time.Parse(time.RFC3339, ts)
+			Expect(err).To(BeNil())
+
+			d := utils.Date(t)
+			Expect(d.Year()).To(Equal(2019))
+			Expect(d.Month()).To(Equal(time.October))
+			Expect(d.Day()).To(Equal(9))
+			Expect(d.Hour()).To(Equal(0))
+			Expect(d.Minute()).To(Equal(0))
+			Expect(d.Second()).To(Equal(0))
+			Expect(d.Nanosecond()).To(Equal(0))
+		})
+	})
 })
