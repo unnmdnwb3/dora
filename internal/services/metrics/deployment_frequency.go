ee2c88aa86c3f96a28cfd3b89eb98ef2801b6253
Janick Rueegger <jrueegger@gmail.com>
2022-12-20 10:23:45 +0100

    Add base service to calculate deployment frequency

---
+++ internal/services/metrics/deployment_frequency.go
@@ -0,0 +1,180 @@
+package metrics
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/unnmdnwb3/dora/internal/daos"
+	"github.com/unnmdnwb3/dora/internal/models"
+	"github.com/unnmdnwb3/dora/internal/utils/times"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// // CalculateDeploymentFrequency calculates the deployment frequency for a given dataflow.
+// func CalculateDeploymentFrequency(ctx context.Context, dataflowID string, window int) (*models.DeploymentFrequency, error) {
+// 	var dataflow models.Dataflow
+// 	err := daos.GetDataflow(ctx, dataflowID, &dataflow)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	pipelineID := dataflow.Pipeline.ID
+// 	timeRange := (window - 1) * 2
+// 	today := times.Date(time.Now())
+// 	startDate := today.AddDate(0, 0, -timeRange)
+
+// 	var pipelineRunsAggregate []models.PipelineRunsPerDay
+// 	filter := bson.M{"pipeline_id": pipelineID, "date": bson.M{"$gte": startDate}}
+// 	err = daos.ListPipelineRunsPerDaysByFilter(ctx, filter, &pipelineRunsAggregate)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	if len(pipelineRunsAggregate) == 0 {
+// 		return nil, fmt.Errorf("no pipeline runs found for pipeline %s", pipelineID)
+// 	}
+
+// 	dates, err := GetDates(&pipelineRunsAggregate)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	dailyPipelineRuns, err := GetDailyPipelineRuns(&pipelineRunsAggregate)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	movingAverages, err := CalculateMovingAverages(dailyPipelineRuns, window)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return &models.DeploymentFrequency{
+// 		DataflowID:        pipelineID,
+// 		Dates:             *dates,
+// 		DailyPipelineRuns: *dailyPipelineRuns,
+// 		MovingAverages:    *movingAverages,
+// 	}, nil
+// }
+
+// CalculateDeploymentFrequency calculates the deployment frequency for a given dataflow.
+func CalculateDeploymentFrequency(ctx context.Context, dataflowID primitive.ObjectID, window int, endDate time.Time) (*models.DeploymentFrequency, error) {
+	var dataflow models.Dataflow
+	err := daos.GetDataflow(ctx, dataflowID, &dataflow)
+	if err != nil {
+		return nil, err
+	}
+
+	offset := window - 1
+	timeRange := offset * 2
+	startDate := times.Date(endDate.AddDate(0, 0, -timeRange))
+
+	var pipelineRunsPerDay []models.PipelineRunsPerDay
+	filter := bson.M{"pipeline_id": dataflow.Pipeline.ID, "date": bson.M{"$gte": startDate}}
+	err = daos.ListPipelineRunsPerDaysByFilter(ctx, filter, &pipelineRunsPerDay)
+	if err != nil {
+		return nil, err
+	}
+	if len(pipelineRunsPerDay) == 0 {
+		return nil, fmt.Errorf("no pipeline runs per days found for pipeline with id: %s", dataflow.Pipeline.ID.Hex())
+	}
+
+	dates, err := DatesBetween(startDate, endDate)
+	if err != nil {
+		return nil, err
+	}
+
+	dailyPipelineRuns, err := CompletePipelineRunsPerDays(&pipelineRunsPerDay, dates)
+	if err != nil {
+		return nil, err
+	}
+
+	movingAverages, err := CalculateMovingAverages(dailyPipelineRuns, window)
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.DeploymentFrequency{
+		DataflowID:        dataflow.ID,
+		Dates:             (*dates)[offset:],
+		DailyPipelineRuns: (*dailyPipelineRuns)[offset:],
+		MovingAverages:    *movingAverages,
+	}, nil
+}
+
+// DatesBetween returns a slice of dates between the start and end dates.
+func DatesBetween(startDate time.Time, endDate time.Time) (*[]time.Time, error) {
+	startDate = times.Date(startDate)
+	endDate = times.Date(endDate)
+
+	if startDate.After(endDate) {
+		return nil, fmt.Errorf("start date is after end date")
+	}
+
+	dates := []time.Time{}
+	for date := startDate; date.Before(endDate); date = date.AddDate(0, 0, 1) {
+		dates = append(dates, times.Date(date))
+	}
+	dates = append(dates, times.Date(endDate))
+
+	return &dates, nil
+}
+
+// CompletePipelineRunsPerDays returns a slice of the number of pipeline runs per day,
+// since provided PipelineRunsPerDays only account for the dates that any pipeline runs were found.
+func CompletePipelineRunsPerDays(pipelineRunsPerDays *[]models.PipelineRunsPerDay, dates *[]time.Time) (*[]int, error) {
+	if len(*pipelineRunsPerDays) == 0 {
+		return nil, fmt.Errorf("no pipeline runs aggregates provided")
+	}
+	if len(*dates) == 0 {
+		return nil, fmt.Errorf("no dates provided pipeline runs aggregates")
+	}
+	if len(*dates) <= len(*pipelineRunsPerDays) {
+		return nil, fmt.Errorf("more pipeline runs per day than dates provided")
+	}
+	if (*dates)[0].Before((*pipelineRunsPerDays)[0].Date) {
+		return nil, fmt.Errorf("first date is before first pipeline runs aggregate date")
+	}
+	if (*dates)[len(*dates)-1].After((*pipelineRunsPerDays)[len(*pipelineRunsPerDays)-1].Date) {
+		return nil, fmt.Errorf("last date is after last pipeline runs aggregate date")
+	}
+
+	dailyPipelineRuns := make([]int, len(*dates))
+	curr := 0
+	for index, date := range *dates {
+		if date == (*pipelineRunsPerDays)[curr].Date {
+			dailyPipelineRuns[index] = (*pipelineRunsPerDays)[curr].TotalPipelineRuns
+			curr++
+		} else {
+			dailyPipelineRuns[index] = 0
+		}
+	}
+
+	return &dailyPipelineRuns, nil
+}
+
+// CalculateMovingAverages calculates the moving averages for a given slice of deployments per day.
+func CalculateMovingAverages(pipelineRunsPerDays *[]int, window int) (*[]float64, error) {
+	if len(*pipelineRunsPerDays) == 0 {
+		return nil, fmt.Errorf("no deployments per day provided to calculate moving averages")
+	}
+	if len(*pipelineRunsPerDays) != (window*2)-1 {
+		return nil, fmt.Errorf("number of pipeline runs per day provided to calculate moving averages does not match window")
+	}
+
+	offset := window - 1
+	totalDeploymentsInWindow := 0
+	for index := 0; index < offset; index++ {
+		totalDeploymentsInWindow += (*pipelineRunsPerDays)[index]
+	}
+
+	movingAverages := make([]float64, len(*pipelineRunsPerDays)-offset)
+	for index := offset; index < len(*pipelineRunsPerDays); index++ {
+		totalDeploymentsInWindow += (*pipelineRunsPerDays)[index]
+		movingAverages[index-offset] = float64(totalDeploymentsInWindow) / float64(window)
+		totalDeploymentsInWindow -= (*pipelineRunsPerDays)[index-offset]
+	}
+
+	return &movingAverages, nil
+}
---
+++ internal/services/metrics/deployment_frequency_test.go
@@ -0,0 +1,231 @@
+package metrics_test
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/unnmdnwb3/dora/internal/daos"
+	"github.com/unnmdnwb3/dora/internal/database/mongodb"
+	"github.com/unnmdnwb3/dora/internal/models"
+	"github.com/unnmdnwb3/dora/internal/services/metrics"
+	"github.com/unnmdnwb3/dora/internal/services/trigger"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var _ = Describe("services.metrics", func() {
+	var (
+		ctx        = context.Background()
+		externalID = 40649465
+	)
+
+	var _ = BeforeEach(func() {
+		_ = godotenv.Load("./../../../test/.env")
+	})
+
+	var _ = AfterEach(func() {
+		ctx := context.Background()
+		service := mongodb.NewService()
+		service.Connect(ctx, os.Getenv("MONGODB_DATABASE"))
+		service.DB.Drop(ctx)
+		defer service.Disconnect(ctx)
+
+		os.Remove("GITLAB_BEARER")
+		os.Remove("GITLAB_URI")
+		os.Remove("MONGODB_URI")
+		os.Remove("MONGODB_PORT")
+		os.Remove("MONGODB_USER")
+		os.Remove("MONGODB_PASSWORD")
+	})
+
+	var _ = When("CalculateMovingAverages", func() {
+		It("returns a list of MovingAverages.", func() {
+			deploymentsPerDay := []int{1, 2, 3, 4, 5}
+
+			movingAverages, err := metrics.CalculateMovingAverages(&deploymentsPerDay, 3)
+			Expect(err).To(BeNil())
+			Expect(len(*movingAverages)).To(Equal(3))
+			Expect((*movingAverages)[0]).To(Equal(2.0))
+			Expect((*movingAverages)[1]).To(Equal(3.0))
+			Expect((*movingAverages)[2]).To(Equal(4.0))
+		})
+	})
+
+	var _ = When("DatesBetween", func() {
+		It("returns a list of dates between two dates.", func() {
+			startDate, _ := time.Parse(time.RFC3339, "2020-02-04T14:29:50.092Z")
+			endDate, _ := time.Parse(time.RFC3339, "2020-02-10T15:29:50.092Z")
+
+			dates, err := metrics.DatesBetween(startDate, endDate)
+			Expect(err).To(BeNil())
+			Expect(len(*dates)).To(Equal(7))
+		})
+
+		It("returns an error", func() {
+			startDateAfterEndDate, _ := time.Parse(time.RFC3339, "2020-02-06T14:29:50.092Z")
+			endDate, _ := time.Parse(time.RFC3339, "2020-02-04T14:29:50.092Z")
+
+			_, err := metrics.DatesBetween(startDateAfterEndDate, endDate)
+			Expect(err).To(Not(BeNil()))
+		})
+	})
+
+	var _ = When("CompleteDailyPipelineRuns", func() {
+		It("returns the complete list of daily pipeline runs between two dates.", func() {
+			pipelineID := primitive.NewObjectID()
+			date1, _ := time.Parse(time.RFC3339, "2020-02-04T00:00:00.000Z")
+			date2, _ := time.Parse(time.RFC3339, "2020-02-05T00:00:00.000Z")
+			date3, _ := time.Parse(time.RFC3339, "2020-02-06T00:00:00.000Z")
+
+			dates := []time.Time{date1, date2, date3}
+			persistedDailyPipelineRuns := []models.PipelineRunsPerDay{
+				{
+					PipelineID:        pipelineID,
+					Date:              date1,
+					TotalPipelineRuns: 1,
+				},
+				{
+					PipelineID:        pipelineID,
+					Date:              date3,
+					TotalPipelineRuns: 1,
+				},
+			}
+
+			completeDailyPipelineRuns, err := metrics.CompletePipelineRunsPerDays(&persistedDailyPipelineRuns, &dates)
+			Expect(err).To(BeNil())
+			Expect(len(*completeDailyPipelineRuns)).To(Equal(3))
+			Expect((*completeDailyPipelineRuns)[0]).To(Equal(1))
+			Expect((*completeDailyPipelineRuns)[1]).To(Equal(0))
+			Expect((*completeDailyPipelineRuns)[2]).To(Equal(1))
+		})
+	})
+
+	var _ = When("CalculateDeploymentFrequency", func() {
+		It("calculates the DeploymentFrequency for a given window.", func() {
+			// create a new dataflow
+			repository := models.Repository{
+				IntegrationID:  primitive.NewObjectID(),
+				ExternalID:     externalID,
+				NamespacedName: "foobar/foobar",
+				DefaultBranch:  "main",
+				URI:            "https://gitlab.com/foobar/foobar",
+			}
+			pipeline := models.Pipeline{
+				IntegrationID:  primitive.NewObjectID(),
+				ExternalID:     externalID,
+				NamespacedName: "foobar/foobar",
+				DefaultBranch:  "main",
+				URI:            "https://gitlab.com/foobar/foobar/-/pipelines",
+			}
+			deployment := models.Deployment{
+				IntegrationID: primitive.NewObjectID(),
+				TargetURI:     "https://localhost:9090",
+			}
+			dataflow := models.Dataflow{
+				Repository: repository,
+				Pipeline:   pipeline,
+				Deployment: deployment,
+			}
+			err := daos.CreateDataflow(ctx, &dataflow)
+			Expect(err).To(BeNil())
+
+			// create pipeline runs
+			createdAt1, _ := time.Parse(time.RFC3339, "2020-02-03T14:29:50.092Z")
+			updatedAt1, _ := time.Parse(time.RFC3339, "2020-02-03T14:35:51.459Z")
+			pipelineRun1 := models.PipelineRun{
+				PipelineID:  dataflow.Pipeline.ID,
+				ExternalID:  713437220,
+				Ref:         "main",
+				Status:      "success",
+				EventSource: "push",
+				CreatedAt:   createdAt1,
+				UpdatedAt:   updatedAt1,
+				URI:         "https://gitlab.com/foobar/foobar/-/pipelines/114884002",
+			}
+
+			createdAt2, _ := time.Parse(time.RFC3339, "2020-02-04T14:29:50.092Z")
+			updatedAt2, _ := time.Parse(time.RFC3339, "2020-02-04T14:35:51.459Z")
+			pipelineRun2 := models.PipelineRun{
+				PipelineID:  dataflow.Pipeline.ID,
+				ExternalID:  713437220,
+				Ref:         "main",
+				Status:      "success",
+				EventSource: "push",
+				CreatedAt:   createdAt2,
+				UpdatedAt:   updatedAt2,
+				URI:         "https://gitlab.com/foobar/foobar/-/pipelines/114884002",
+			}
+
+			createdAt3, _ := time.Parse(time.RFC3339, "2020-02-04T15:29:50.092Z")
+			updatedAt3, _ := time.Parse(time.RFC3339, "2020-02-04T15:35:51.459Z")
+			pipelineRun3 := models.PipelineRun{
+				PipelineID:  dataflow.Pipeline.ID,
+				ExternalID:  713437221,
+				Ref:         "main",
+				Status:      "success",
+				EventSource: "push",
+				CreatedAt:   createdAt3,
+				UpdatedAt:   updatedAt3,
+				URI:         "https://gitlab.com/foobar/foobar/-/pipelines/114884003",
+			}
+
+			createdAt4, _ := time.Parse(time.RFC3339, "2020-02-06T14:29:50.092Z")
+			updatedAt4, _ := time.Parse(time.RFC3339, "2020-02-06T14:35:51.459Z")
+			pipelineRun4 := models.PipelineRun{
+				PipelineID:  dataflow.Pipeline.ID,
+				ExternalID:  713437222,
+				Ref:         "main",
+				Status:      "success",
+				EventSource: "push",
+				CreatedAt:   createdAt4,
+				UpdatedAt:   updatedAt4,
+				URI:         "https://gitlab.com/foobar/foobar/-/pipelines/114884004",
+			}
+
+			createdAt5, _ := time.Parse(time.RFC3339, "2020-02-07T14:29:50.092Z")
+			updatedAt5, _ := time.Parse(time.RFC3339, "2020-02-07T14:35:51.459Z")
+			pipelineRun5 := models.PipelineRun{
+				PipelineID:  dataflow.Pipeline.ID,
+				ExternalID:  713437223,
+				Ref:         "main",
+				Status:      "success",
+				EventSource: "push",
+				CreatedAt:   createdAt5,
+				UpdatedAt:   updatedAt5,
+				URI:         "https://gitlab.com/foobar/foobar/-/pipelines/114884005",
+			}
+
+			createdAt6, _ := time.Parse(time.RFC3339, "2020-02-07T15:29:50.092Z")
+			updatedAt6, _ := time.Parse(time.RFC3339, "2020-02-07T15:35:51.459Z")
+			pipelineRun6 := models.PipelineRun{
+				PipelineID:  dataflow.Pipeline.ID,
+				ExternalID:  713437224,
+				Ref:         "main",
+				Status:      "success",
+				EventSource: "push",
+				CreatedAt:   createdAt6,
+				UpdatedAt:   updatedAt6,
+				URI:         "https://gitlab.com/foobar/foobar/-/pipelines/114884006",
+			}
+			pipelineRuns := []models.PipelineRun{pipelineRun1, pipelineRun2, pipelineRun3, pipelineRun4, pipelineRun5, pipelineRun6}
+			err = daos.CreatePipelineRuns(ctx, dataflow.Pipeline.ID, &pipelineRuns)
+			Expect(err).To(BeNil())
+
+			// create pipeline runs aggregates
+			pipelineRunsPerDays, err := trigger.CalculatePipelineRunsPerDays(ctx, &pipelineRuns)
+			Expect(err).To(BeNil())
+			err = daos.CreatePipelineRunsPerDays(ctx, dataflow.Pipeline.ID, pipelineRunsPerDays)
+			Expect(err).To(BeNil())
+
+			// actions above are assumed to be carried out by the trigger, when a new dataflow is created
+			deploymentFrequency, err := metrics.CalculateDeploymentFrequency(ctx, dataflow.ID, 3, createdAt5)
+			Expect(err).To(BeNil())
+			Expect(deploymentFrequency.DataflowID).To(Equal(dataflow.ID))
+			Expect(deploymentFrequency.MovingAverages).To(Equal([]float64{1.0, 1.0, 1.0}))
+			Expect(deploymentFrequency.DailyPipelineRuns).To(Equal([]int{0, 1, 2}))
+		})
+	})
+})
