a0b775d55895999c94e389cd231eef8046699abb
Janick Rueegger <jrueegger@gmail.com>
2022-12-25 10:42:47 +0100

    Add total http metrics

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,38 +1,68 @@
 package api
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/unnmdnwb3/dora/internal/api/handler"
 )
+
+// httpRequestsTotal is a prometheus counter for all HTTP requests
+var httpRequestsTotal = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "Number of requests.",
+	},
+	[]string{"method", "path", "code"},
+)
+
+// registerMetrics registers all metrics to be served
+func registerMetrics() {
+	prometheus.MustRegister(httpRequestsTotal)
+	return
+}
+
+// prometheusMiddleware is a middleware to set up all custom prometheus metrics
+func prometheusMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+
+		httpRequestsTotal.WithLabelValues(c.Request.Method, c.Request.URL.Path, strconv.Itoa(c.Writer.Status())).Inc()
+	}
+}
 
 // SetupRouter initializes the router and all routes to be served
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 
+	// register prometheus metrics
+	registerMetrics()
+
 	// route for prometheus metrics
-	router.GET("/healthz", handler.Healthz)
-	router.GET("/metrics", gin.WrapH(promhttp.Handler()))
+	router.GET("/healthz", prometheusMiddleware(), handler.Healthz)
+	router.GET("/metrics", prometheusMiddleware(), gin.WrapH(promhttp.Handler()))
 
 	// routes for repositories
-	router.GET("/api/v1/repositories", handler.GetRepositories)
+	router.GET("/api/v1/repositories", prometheusMiddleware(), handler.GetRepositories)
 
 	// routes for integrations
-	router.POST("/api/v1/integrations", handler.CreateIntegration)
-	router.GET("/api/v1/integrations", handler.ListIntegrations)
-	router.GET("/api/v1/integrations/:id", handler.GetIntegration)
-	router.PUT("/api/v1/integrations/:id", handler.UpdateIntegration)
-	router.DELETE("/api/v1/integrations/:id", handler.DeleteIntegration)
+	router.POST("/api/v1/integrations", prometheusMiddleware(), handler.CreateIntegration)
+	router.GET("/api/v1/integrations", prometheusMiddleware(), handler.ListIntegrations)
+	router.GET("/api/v1/integrations/:id", prometheusMiddleware(), handler.GetIntegration)
+	router.PUT("/api/v1/integrations/:id", prometheusMiddleware(), handler.UpdateIntegration)
+	router.DELETE("/api/v1/integrations/:id", prometheusMiddleware(), handler.DeleteIntegration)
 
 	// routes for dataflows
-	router.POST("/api/v1/dataflows", handler.CreateDataflow)
-	router.GET("/api/v1/dataflows", handler.ListDataflows)
-	router.GET("/api/v1/dataflows/:id", handler.GetDataflow)
-	router.PUT("/api/v1/dataflows/:id", handler.UpdateDataflow)
-	router.DELETE("/api/v1/dataflows/:id", handler.DeleteDataflow)
+	router.POST("/api/v1/dataflows", prometheusMiddleware(), handler.CreateDataflow)
+	router.GET("/api/v1/dataflows", prometheusMiddleware(), handler.ListDataflows)
+	router.GET("/api/v1/dataflows/:id", prometheusMiddleware(), handler.GetDataflow)
+	router.PUT("/api/v1/dataflows/:id", prometheusMiddleware(), handler.UpdateDataflow)
+	router.DELETE("/api/v1/dataflows/:id", prometheusMiddleware(), handler.DeleteDataflow)
 
 	// routes for metrics
-	router.POST("/api/v1/metrics/deployment-frequency", handler.DeploymentFrequency)
+	router.POST("/api/v1/metrics/deployment-frequency", prometheusMiddleware(), handler.DeploymentFrequency)
 
 	return router
 }
