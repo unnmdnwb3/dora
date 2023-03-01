3ab06926875134ba26cd9d2bade96868c75a877f
Janick Rueegger <jrueegger@gmail.com>
2023-03-01 16:30:38 +0100

    Remove monitoring data model

--- internal/models/monitoring_data_point.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "time"
-
-// MonitoringDataPoint describes a single data point in a monitoring time series.
-type MonitoringDataPoint struct {
-	Value     float64   `json:"value"`
-	CreatedAt time.Time `json:"created_at"`
-}
